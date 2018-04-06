72902046b373d4f5373a2897aaafd868c0a4b6bd
Sagar <Sagar@Sagarkhalashi_QA>
2018-04-06 14:08:38 +0530

    Driver setup

--- AutomationQA1/src/main/java/Framework/ExecutionSetup.java
+++ AutomationQA1/src/main/java/Framework/ExecutionSetup.java
@@ -2,6 +2,7 @@ package Framework;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
